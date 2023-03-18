adec062ac3a58df6310782f0e0f6e9b10db2fb8a
pitziro <msarmientor1@gmail.com>
2023-03-18 01:40:53 -0500

    commit algo responsive manual v2

--- src/components/top/NavItem.jsx
+++ src/components/top/NavItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Link} from 'react-router-dom'
 
 import {ExpandMoreOutlined, ExpandLessOutlined} from '@mui/icons-material';
@@ -11,7 +11,7 @@ export default function NavItem(props) {
     const {item, handleClick, barOpened} = props
     const [opened, setopen] = useState(false);
 
-    const handleOpen = () => {
+    const handleOpenSub = () => {
         setopen (prev => !prev)
     }
 
@@ -19,24 +19,44 @@ export default function NavItem(props) {
         setopen (false)
     }, [barOpened]);
 
+    const navRefSub = useRef(null)
+    
+    useEffect(() => {
+        const clickOutside = (evt) => {
+            if ((navRefSub.current) && (!navRefSub.current.contains(evt.target))) {
+                setopen(false)
+            }
+        }
+        document.addEventListener('mousedown',clickOutside)
+        
+        return () => {
+            document.removeEventListener('mousedown',clickOutside)
+        };
+    }, [navRefSub]);
+
+
+
+
     if (item.childrens){
         return (
-            <div className={mstyle.lista_menu}>
-                <div className={mstyle.lista_simple} onClick={handleOpen}>
-                        <a href="#">{item.title}</a>
-                        {opened ? <ExpandLessOutlined/> : <ExpandMoreOutlined/>}
+            <div className={mstyle.categoryitem}> 
+                <div className={mstyle.subcategory_title} onClick={handleOpenSub}>
+                    <a href="#">{item.title}</a>
+                    {opened ? <ExpandLessOutlined/> : <ExpandMoreOutlined/>}
                 </div>
-                <div className={opened ? `${mstyle.lista_content} ${mstyle.opened}` : `${mstyle.lista_content}`}>
+
+                <div ref={navRefSub} className={opened ? `${mstyle.subcategory_container} ${mstyle.opened}` : `${mstyle.subcategory_container} `}>
+                {/* <div className={`${mstyle.subcategory_container} ${mstyle.opened}`}> */}
                     { item.childrens.map((child, index) => <NavItem key={index} item={child} handleClick={handleClick}/>) }
                 </div>
-            </div>
+            </div> 
         )
     } 
     
     else
     {
         return (
-            <div className={mstyle.lista_simple}> 
+            <div className={mstyle.categoryitem}> 
                 <Link to={item.path} onClick={handleClick}> {item.title} </Link>
             </div>
         )
