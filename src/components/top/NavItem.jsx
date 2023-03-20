fd32144ae720640af25ae8daeebd2b346431685a
pitziro <msarmientor1@gmail.com>
2023-03-19 23:04:23 -0500

    commit algo responsive manual v3

--- src/components/top/NavItem.jsx
+++ src/components/top/NavItem.jsx
@@ -1,65 +1,74 @@
-import React, {useState, useEffect, useRef} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
-import {ExpandMoreOutlined, ExpandLessOutlined} from '@mui/icons-material';
+import { ExpandMoreOutlined, ExpandLessOutlined } from '@mui/icons-material';
 
-import mstyle from './Navbar.module.css'
+import mstyle from './Navbar.module.css';
 
 export default function NavItem(props) {
-    
+	const { item, handleClick, barOpened } = props;
 
-    const {item, handleClick, barOpened} = props
-    const [opened, setopen] = useState(false);
+	const [subcategoryOpened, setSubcategoryOpened] = useState(false);
+	const handleOpenSub = () => {
+		setSubcategoryOpened((prev) => !prev);
+	};
 
-    const handleOpenSub = () => {
-        setopen (prev => !prev)
-    }
+	const navRefSub = useRef(null);
 
-    useEffect(() => {
-        setopen (false)
-    }, [barOpened]);
+	useEffect(() => {
+		setSubcategoryOpened(false);
+	}, [barOpened]);
 
-    const navRefSub = useRef(null)
-    
-    useEffect(() => {
-        const clickOutside = (evt) => {
-            if ((navRefSub.current) && (!navRefSub.current.contains(evt.target))) {
-                setopen(false)
-            }
-        }
-        document.addEventListener('mousedown',clickOutside)
-        
-        return () => {
-            document.removeEventListener('mousedown',clickOutside)
-        };
-    }, [navRefSub]);
+	useEffect(() => {
+		const clickOutside = (evt) => {
+			if (
+				navRefSub.current &&
+				!navRefSub.current.contains(evt.target) &&
+				!subcategoryOpened
+			) {
+				setSubcategoryOpened(false);
+			}
+		};
+		document.addEventListener('mousedown', clickOutside);
 
+		return () => {
+			document.removeEventListener('mousedown', clickOutside);
+		};
+	}, [navRefSub]);
 
+	if (item.childrens) {
+		return (
+			<div
+				ref={navRefSub}
+				className={mstyle.categoryitem}
+				onClick={handleOpenSub}
+			>
+				<div className={mstyle.subcategory_title}>
+					<a href='#'>{item.title}</a>
+					{subcategoryOpened ? <ExpandLessOutlined /> : <ExpandMoreOutlined />}
+				</div>
 
-
-    if (item.childrens){
-        return (
-            <div className={mstyle.categoryitem}> 
-                <div className={mstyle.subcategory_title} onClick={handleOpenSub}>
-                    <a href="#">{item.title}</a>
-                    {opened ? <ExpandLessOutlined/> : <ExpandMoreOutlined/>}
-                </div>
-
-                <div ref={navRefSub} className={opened ? `${mstyle.subcategory_container} ${mstyle.opened}` : `${mstyle.subcategory_container} `}>
-                {/* <div className={`${mstyle.subcategory_container} ${mstyle.opened}`}> */}
-                    { item.childrens.map((child, index) => <NavItem key={index} item={child} handleClick={handleClick}/>) }
-                </div>
-            </div> 
-        )
-    } 
-    
-    else
-    {
-        return (
-            <div className={mstyle.categoryitem}> 
-                <Link to={item.path} onClick={handleClick}> {item.title} </Link>
-            </div>
-        )
-    }
-
+				<div
+					className={
+						subcategoryOpened
+							? `${mstyle.subcategory_container} ${mstyle.opened}`
+							: `${mstyle.subcategory_container} `
+					}
+				>
+					{item.childrens.map((child, index) => (
+						<NavItem key={index} item={child} handleClick={handleClick} />
+					))}
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className={mstyle.categoryitem}>
+				<Link to={item.path} onClick={handleClick}>
+					{' '}
+					{item.title}{' '}
+				</Link>
+			</div>
+		);
+	}
 }
--- src/components/top/Navbar.jsx
+++ src/components/top/Navbar.jsx
@@ -1,61 +1,62 @@
-import React, {useState, useEffect, useRef} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
-import mstyle from './Navbar.module.css'
-import aStyle from '../../App.module.css'
+import mstyle from './Navbar.module.css';
+import aStyle from '../../App.module.css';
 
-import NavItem from './NavItem'
-import Mobile from './Mobile'
+import NavItem from './NavItem';
+import Mobile from './Mobile';
 
-import myitems from "../../data/navbarData.json"
-
+import myitems from '../../data/navbarData.json';
 
 function Navbar() {
+	const [barOpened, setbarOpened] = useState(false);
 
-    const [barOpened, setbarOpened] = useState(false)
-    
-    const handleClick = () => {
-        setbarOpened (prev => !prev)
-    }
+	const handleClick = () => {
+		setbarOpened((prev) => !prev);
+	};
 
-    const navRef = useRef(null);
-    
-    useEffect(() => {
-        const clickOutside = (evt) => {
-            if ((navRef.current) && (!navRef.current.contains(evt.target))) {
-                setbarOpened(false)
-            }
-        }
-        document.addEventListener('mousedown',clickOutside)
-        
-        return () => {
-            document.removeEventListener('mousedown',clickOutside)
-        };
-    }, [navRef]);
+	const navRef = useRef(null);
 
+	useEffect(() => {
+		const clickOutside = (evt) => {
+			if (navRef.current && !navRef.current.contains(evt.target)) {
+				setbarOpened(false);
+			}
+		};
+		document.addEventListener('mousedown', clickOutside);
 
-    return (
-    <div className={aStyle.zonatop}>
-        
-        <div className={mstyle.top_logo} >
-            <Link to="/"> 
-                <img className={mstyle.logo_img} src="/logotipo16.png" title="Home" alt='Home'/> 
-                <img className={mstyle.logo_letras} src='nombre-sin-fondo.png'/>
-            </Link>
-        </div>
+		return () => {
+			document.removeEventListener('mousedown', clickOutside);
+		};
+	}, [navRef]);
 
+	return (
+		<div className={aStyle.zonatop}>
+			<div className={mstyle.top_logo}>
+				<Link to='/'>
+					<img
+						className={mstyle.logo_img}
+						src='/logotipo16.png'
+						title='Home'
+						alt='Home'
+					/>
+					<img className={mstyle.logo_letras} src='nombre-sin-fondo.png' />
+				</Link>
+			</div>
 
-        {/* <nav ref={navRef}  id={mstyle.categorybar} className={barOpened && `${mstyle.show}` } > */}
-        <nav ref={navRef}  id={mstyle.categorybar} className={barOpened ? `${mstyle.show}` : `${mstyle.inicial}`} >
-            {
-            myitems.map((item) => <NavItem key={item.id} item={item} handleClick={handleClick} barOpened={barOpened}/>)
-            }
-        </nav>
-        <Mobile fClick = {{barOpened, handleClick}} />
-
-        
-    </div>
-    )
+			<nav
+				ref={navRef}
+				id={mstyle.categorybar}
+				className={barOpened ? `${mstyle.show}` : `${mstyle.inicial}`}
+			>
+				{myitems.map((item) => (
+					<NavItem key={item.id} {...{ item, handleClick, barOpened }} />
+				))}
+			</nav>
+			<Mobile fClick={{ barOpened, handleClick }} />
+		</div>
+	);
 }
 
-export default Navbar
+export default Navbar;
